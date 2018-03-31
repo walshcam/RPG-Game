8b1a13116ead57b0ffadc0a5b17180f522fac858
walshcam <35083171+walshcam@users.noreply.github.com>
2018-03-31 16:18:23 -0400

    Apparently if statements are subjective

--- javascript/script.js
+++ javascript/script.js
@@ -2,6 +2,21 @@
 var challengerHealthPoints = 0;
 var heroHealthPoints = 0;
 var heroAttackPoints = 0;
+//When a hero or challenger is chosen
+var heroImage = true;
+var challengerImage = true;
+//When the player wins
+var playerCount = 0;
+//When a specific character turns into the hero
+var sheepHero = false;
+var dogHero = false;
+var knightHero = false;
+var dragonHero = false;
+//When a character turns into a challenger
+var sheepChallenger = false;
+var dogChallenger = false;
+var knightChallenger = false;
+var dragonChallenger = false;
 //     <!-- The health points, attack power and counter attack power of each character must differ. Each character has these three attributes -->
 
 
@@ -34,13 +49,13 @@ var heroAttackPoints = 0;
         countAttackPower : 15
     };
 
-console.log("Is this reading?")
+console.log("Is this reading?");
 
 
 //     <!-- When the game starts, the player will choose a character by clicking on the fighter's picture. The player will fight as this character for the rest of the game.-->
 
 $(document).ready(function() {
-console.log("At least its reading...")
+console.log("At least its reading...");
 
 // Initial health points
 
@@ -67,25 +82,27 @@ function chooseHero(){
     $('.heroTextbox').css('background-color', 'black');
     $('.remainingCharacter').css('border','10px solid red');
     $('#comment').html('Choose your opponent!');
+    heroImage = false;
+    playerCount = 1;
     $('#buttonAttack').click(function(){
-        $('#comment').html("The wind is a worthy opponent, but now it is time to fight something a little more solid. Choose your challenger!")
+        $('#comment').html("The wind is a worthy opponent, but now it is time to fight something a little more solid. Choose your challenger!");
     });
 
     // Hero Health Points Input
     if($('#hero').attr('src').indexOf('sheep') != -1) {
         heroHealthPoints = sheep.healthPoints;
-    }
+    };
     if($('#hero').attr('src').indexOf('dog') != -1) {
         heroHealthPoints = neighborsDog.healthPoints;
-    }
+    };
     if($('#hero').attr('src').indexOf('knight') != -1) {
         heroHealthPoints = knight.healthPoints;
-    }
+    };
     if($('#hero').attr('src').indexOf('Dragon') != -1) {
         heroHealthPoints = superEvilDragon.healthPoints;
-    }
+    };
     $('#textboxHero').html(heroHealthPoints);    
-}
+};
 
 //Function used when a challenger is chosen.
 
@@ -95,20 +112,9 @@ function chooseChallenger(){
     $('.challengerTextbox').css('background-color', 'black');
     $('.remainingCharacter').css('border','10px solid white')
     $('#comment').html('Now it is time to fight... TO THE DEATH! (No really, fight to the death.)')
-
-    // Challenger Health Points Input
-    if($('#challenger').attr('src').indexOf('sheep') != -1) {
-        challengerHealthPoints = sheep.healthPoints;
-    }
-    if($('#challenger').attr('src').indexOf('dog') != -1) {
-        challengerHealthPoints = neighborsDog.healthPoints;
-    }
-    if($('#challenger').attr('src').indexOf('knight') != -1) {
-        challengerHealthPoints = knight.healthPoints;
-    }
-    if($('#challenger').attr('src').indexOf('Dragon') != -1) {
-        challengerHealthPoints = superEvilDragon.healthPoints;
-    }
+    challengerImage = false;
+    playerCount++;
+    console.log(playerCount)
     $('#textboxChallenger').html(challengerHealthPoints);
 };
 
@@ -117,7 +123,7 @@ function attackComments(){
     var attackCommentArray = ["You got this! Attack again!","You get a flesh wound! And you get a flesh wound!","Direct hit!","Kill him all the way to death! Attack again!","That was his favorite foot!"];
     var attackComment = Math.floor(Math.random() * attackCommentArray.length);
     $('#comment').html(attackCommentArray[attackComment]);
-}
+};
 
 //Function that 
     //increases the attack power per attack
@@ -133,16 +139,16 @@ function turnAttack() {
         if($('#challenger').attr('src').indexOf('dog') != -1) {
             heroHealthPoints = heroHealthPoints - neighborsDog.countAttackPower;
             $('#textboxHero').html(heroHealthPoints);
-        }
+        };
         if($('#challenger').attr('src').indexOf('Dragon') != -1) {
             heroHealthPoints = heroHealthPoints - superEvilDragon.countAttackPower;
             $('#textboxHero').html(heroHealthPoints);
-        }
+        };
         if($('#challenger').attr('src').indexOf('knight') != -1) {
             heroHealthPoints = heroHealthPoints - knight.countAttackPower;
             $('#textboxHero').html(heroHealthPoints);
-        }
-    }
+        };
+    };
     //*******DOG ATTACK */
     if($('#hero').attr('src').indexOf('dog') != -1) {
         attackComments()
@@ -152,16 +158,16 @@ function turnAttack() {
         if($('#challenger').attr('src').indexOf('sheep') != -1) {
             heroHealthPoints = heroHealthPoints - neighborsDog.countAttackPower;
             $('#textboxHero').html(heroHealthPoints);
-        }
+        };
         if($('#challenger').attr('src').indexOf('Dragon') != -1) {
             heroHealthPoints = heroHealthPoints - superEvilDragon.countAttackPower;
             $('#textboxHero').html(heroHealthPoints);
-        }
+        };
         if($('#challenger').attr('src').indexOf('knight') != -1) {
             heroHealthPoints = heroHealthPoints - knight.countAttackPower;
             $('#textboxHero').html(heroHealthPoints);
-        }
-    }
+        };
+    };
     //*******KNIGHT ATTACK */
     if($('#hero').attr('src').indexOf('knight') != -1) {
         attackComments()
@@ -171,16 +177,16 @@ function turnAttack() {
         if($('#challenger').attr('src').indexOf('sheep') != -1) {
             heroHealthPoints = heroHealthPoints - sheep.countAttackPower;
             $('#textboxHero').html(heroHealthPoints);
-        }
+        };
         if($('#challenger').attr('src').indexOf('Dragon') != -1) {
             heroHealthPoints = heroHealthPoints - superEvilDragon.countAttackPower;
             $('#textboxHero').html(heroHealthPoints);
-        }
+        };
         if($('#challenger').attr('src').indexOf('dog') != -1) {
             heroHealthPoints = heroHealthPoints - neighborsDog.countAttackPower;
             $('#textboxHero').html(heroHealthPoints);
-        }
-    }
+        };
+    };
     //*******SUPER EVIL DRAGON ATTACK */
     if($('#hero').attr('src').indexOf('Dragon') != -1) {
         attackComments()
@@ -190,45 +196,45 @@ function turnAttack() {
         if($('#challenger').attr('src').indexOf('sheep') != -1) {
             heroHealthPoints = heroHealthPoints - sheep.countAttackPower;
             $('#textboxHero').html(heroHealthPoints);
-        }
+        };
         if($('#challenger').attr('src').indexOf('knight') != -1) {
             heroHealthPoints = heroHealthPoints - knight.countAttackPower;
             $('#textboxHero').html(heroHealthPoints);
-        }
+        };
         if($('#challenger').attr('src').indexOf('dog') != -1) {
             heroHealthPoints = heroHealthPoints - neighborsDog.countAttackPower;
             $('#textboxHero').html(heroHealthPoints);
-        }
-    }
-}
+        };
+    };
+};
 
 //Function when player wins match
 
 function win() {
     $("#challenger").attr('src','images/placeholder.jpg'); 
-    $('#comment').html("Choose your next challenger")
+    $('#comment').html("Choose your next challenger");
     attackButton();
 }
 
 //Function when player wins game
 
 function ultimateWin() {
-    $('#buttonAttack').html('RESET')
-    $('#comment').html("You are the champion! There's no time for losers because you are the champion.... of the world!!!")
+    $('#buttonAttack').html('RESET');
+    $('#comment').html("You are the champion! There's no time for losers because you are the champion.... of the world!!!");
     $('#buttonAttack').click(function(){
         lossAfterReset();
     });
-}
+};
 
 //Function when player loses
 
 function loss() {
-    $('#buttonAttack').html('RESET')
-    $('#comment').html("Hero has fainted! Please try again... this is a pay-by-click website.")
+    $('#buttonAttack').html('RESET');
+    $('#comment').html("Hero has fainted! Please try again... this is a pay-by-click website.");
     $('#buttonAttack').click(function(){
         lossAfterReset();
     });
-}
+};
 
 function lossAfterReset () {
     //****Attack Button */
@@ -248,9 +254,11 @@ function lossAfterReset () {
     challengerHealthPoints = 0;
     heroHealthPoints = 0;
     heroAttackPoints = 0;
+    heroImage = true;
+    challengerImage = true;
     //*****UNHIDE CHARACTERS */
     $('#character1, #character2, #character3, #character4').show();
-}
+};
 
 
 //********************************GAME ALGORITHM************************************* */
@@ -263,63 +271,69 @@ function lossAfterReset () {
 
 
     $("#character1Picture").click(function(){
-        if ($('#hero').attr('src').indexOf('placeholder') != -1) {
+        if (heroImage) {
             $(this).parent().hide();
             $("#hero").attr('src','images/sheep.jpg');
             chooseHero();
-            console.log($('#challenger').attr('src').indexOf('placeholder'));
-        }
+            heroHealthPoints = sheep.healthPoints;
+            console.log('Clicked on Hero Image')
+        };
 
             //*****CHOOSE CHALLENGER - DOG */
 
-        if ($('#challenger').attr('src').indexOf('placeholder') != -1) {
+        if (challengerImage) {
+            console.log("Get That Dog" + $('#challenger').attr('src').indexOf('placeholder'));
             $('#character2Picture').click(function(){
                 console.log("Second Level Function");
                 $(this).parent().hide();
                 $("#challenger").attr('src','images/dog.jpeg');
                 chooseChallenger();
+                challengerHealthPoints = neighborsDog.healthPoints;
             });
-        }
+        };
 
             //*****CHOOSE CHALLENGER - KNIGHT */
 
-        if ($('#challenger').attr('src').indexOf('placeholder') != -1) {
+        if (challengerImage) {
             $('#character3Picture').click(function(){
                 console.log("Second Level Function");
                 $(this).parent().hide();
                 $("#challenger").attr('src','images/knight.JPG');
                 chooseChallenger();
+                challengerHealthPoints = knight.healthPoints;
             });
-        }
+        };
 
             //*****CHOOSE CHALLENGER - DRAGON */
 
-        if ($('#challenger').attr('src').indexOf('placeholder') != -1) {
+        if (challengerImage) {
             $('#character4Picture').click(function(){
                 console.log("Second Level Function");
                 $(this).parent().hide();
                 $("#challenger").attr('src','images/superEvilDragon.jpg');
                 chooseChallenger();
+                challengerHealthPoints = superEvilDragon.healthPoints;
             });
-        }
+        };
     });        
 
             //******ATTACK */
 
     $('#buttonAttack').click(function(){
-        console.log($('#challenger').attr('src').indexOf('placeholder'))
-        if ($('#challenger').attr('src').indexOf('placeholder') === -1) {
+        if (challengerImage === false) {
+            console.log(playerCount);
+            console.log("TurnAttack Activated");
             turnAttack();
             if (heroHealthPoints <= 0) {
                 loss();
-            }
-            if($('#row5').children(':visible').length === 0) {
+            };
+            if(playerCount ===4) {
                 ultimateWin();
-            }
+            };
             if (challengerHealthPoints <= 0){
                 win();
-            }
-        }
+            };
+        };
     });
 
 
